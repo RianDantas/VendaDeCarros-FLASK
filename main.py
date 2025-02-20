6361d360fc067f1fdbe6c69262608fdf1650ce19
RianDantas <134387076+RianDantas@users.noreply.github.com>
2025-02-20 17:24:21 +0000

    Funcionalidades nos templates

--- main.py
+++ main.py
@@ -20,4 +20,27 @@ def realizarVendas():
     return render_template("realizarVendas.html")
 
 
+@app.route("/salvarCarros", methods=['POST'])
+def salvarCarros():
+    modelo = request.form.get("modelo")
+    marca = request.form.get("marca")
+    fabricacao = request.form.get("fabricacao")
+    quilometragem = str(request.form.get("quilometragem"))
+
+    mensagem = "Marca: " + marca + "," + " Modelo: " + modelo + "," + " ano de fabricação: " + fabricacao + ", " + "Quilometragem: " + quilometragem
+    return render_template("cadastrarCarros.html", modelo = mensagem)
+
+@app.route("/salvarClientes", methods=['POST'])
+def salvarClientes():
+    nome = request.form.get("nome")
+    telefone = request.form.get("telefone")
+    email = request.form.get("email")
+
+    mensagem = "nome: " + nome + "," + "telefone: " + telefone + "," + " ano de fabricação: " + email 
+    return render_template("cadastrarClientes.html", modelo = mensagem)
+
+
+
+
+
 app.run(debug=True)
